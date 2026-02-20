cc7fe7e4e6fc862c6fd39d9930fc85560c4e9a75
Rim <rimtarhini@gmail.com>
2026-02-20 13:05:03 +0200

    changed the app name

--- lib/app.dart
+++ lib/app.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Tensorflow Demo',
+      title: 'Envision',
       navigatorKey: NavigationService.instance.key,
       theme: ThemeData(
         useMaterial3: true,
